42d9b7ae1f2d5ad4efaf6ceaf5e4de671ea1e4ed
d4wae89d498 <faussurier.marc@icloud.com>
2022-12-28 15:11:57 +0100

    Update hook.js

--- hook.js
+++ hook.js
@@ -3,13 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   hook.js                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mafaussu <mafaussu@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: mafaussu <mafaussu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/30 12:00:00 by mafaussu          #+#    #+#             */
 /*   Updated: 2022/06/30 12:42:21 by mafaussu         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
+console.log("hook::init");
 function hook(target, replacement)
 {
     const original_function = target;
